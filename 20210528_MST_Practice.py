33d3eaa14266ee6160fbf6dc431cc2ce5a2a540d
seungrokYoon <sonicrok93@gmail.com>
2021-06-16 18:09:33 +0900

    Prim's MST

--- 20210528_MST_Practice.py
+++ 20210528_MST_Practice.py
@@ -167,5 +167,88 @@ https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-al
 """
 2. Prim's MST Algorithm
 https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/
+구현 연습
+https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1
 
+궁금증: 그렇다면 Prim's Algorithm 에서는 사이클 판별을 어떻게 하는 것일까? 자동으로 사이클이 판별이 되나?
+궁금증2: 시간 복잡도는 adj matrix - O(V^^2), adj list - O(E*LogV)
 """
+#Prim's algorithm with adj matrix
+
+#Prim's algorithm with adj list + full implementation of heap
+from collections import defaultdict
+import sys
+class Heap:
+    def __init__(self):
+        self.array=[]
+        self.size=[]
+        self.pos=[]
+
+    def newMinHeapNode(self,v,dist):
+        minHeapNode = [v,dist]
+        return minHeapNode
+
+    def swapMinHeapNode(self,a,b):
+        t=self.array[a]
+        self.array[a] = self.array[b]
+        self.array[b] = t
+
+    def minHeapify(self,idx):
+        smallest= idx
+        left= 2*idx +1
+        right = 2*idx +2
+        if left <self.size and self.array[left][1] < self.array[smallest][1]:
+            smallest =left
+        if right <self.size and self.array[right][1] < self.array[smallest][1]:
+            smallest = right
+
+        if smallest !=idx:
+            self.pos[self.array[smallest][0]] =idx
+            self.pos[self.array[idx][0]] = smallest
+
+            self.swapMinHeapNode(smallest, idx)
+            self.minHEapify(smallest)
+
+    def extractMin(self):
+        if self.isEmpty() ==True:
+            return
+        root= self.array[0]
+        lastNode= self.array[self.size -1]
+        self.array[0] = lastNode
+        self.pos[lastNode[0]] = 0
+        self.pos[root[0]] = self.size-1
+
+        self.size -=1
+        self.minHeapify(0)
+        return root
+
+    def isEmpty(self):
+        return True if self.size ==0 else False
+
+    def decreaseKey(self,v,dist):
+        i=self.pos[v]
+        self.array[i][1] = dist
+        while i>0 and self.array[i][1] < self.array[(i-1)/2][1]:
+            self.pos[self.array[i][0]] = (i-1)/2
+            self.pos[self.array[(i-2)/2][0]] = i
+            self.swapMinHeapNode(i, (i-1)/2)
+            i= (i-1)/2
+
+    def isInMinHeap(self,v):
+        if self.pos[v] <self.size:
+            return True
+        return False
+
+def printArr(parent, n):
+    for i in range(1,n):
+        print("%d -%d" %(parent[i],i))
+
+class Graph:
+    def __init__(self,V):
+        self.V=V
+        self.graph = defaultdict(list)
+
+
+
+
+
---
+++ 20210616_MST_Prim.py
@@ -0,0 +1,45 @@
+#Prim's MST
+
+import sys
+from collections import defaultdict
+import heapq
+input = sys.stdin.readline
+
+
+class Graph:
+    def __init__(self, n):
+        self.V = n
+        self.adj_list = defaultdict(list)
+
+    def addEdge(self, u, v, w):
+        # undirected니까
+        self.adj_list[u].append((w, v))
+        self.adj_list[v].append((w, u))
+
+    def prim(self):
+        cost=0
+        visited=set()
+        visited.add(1)
+        heap =[ _ for _ in self.adj_list[1]]
+        heapq.heapify(heap)
+        while heap and len(visited)!= (self.V):
+            dist, neighbor= heapq.heappop(heap)
+
+            if neighbor not in visited:
+                print(neighbor)
+                visited.add(neighbor)
+                cost+=dist
+                for new_dist, new_neighbor in self.adj_list[neighbor]:
+                    if new_neighbor not in visited:
+                        heapq.heappush(heap,(new_dist, new_neighbor))
+
+        return cost
+
+#n정점개수 m간선개수
+n,m = map(int, (input().rstrip().split()))
+graph = Graph(n)
+for _ in range(m):
+    u, v, w = list(map(int, input().rstrip().split()))
+    graph.addEdge(u, v, w)
+
+print(graph.prim())
